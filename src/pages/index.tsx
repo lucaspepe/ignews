de18746986778d7dbbbc6ab4fa235fa2e5b3ad75
Lucas Carvalho <lukas_pe_pe@hotmail.com>
2022-04-15 12:36:22 -0300

    stripe inclusion

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,24 @@
+import { GetStaticProps } from 'next';
 import Image from 'next/image';
 import Head from 'next/head';
 
 import styles from './home.module.scss';
 import girlCoding from '../../public/images/avatar.svg';
 import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
 
-export default function Home() {
+//Chamadas client-side
+//Chamadas server-side
+//Chamadas static site generation
+
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -17,13 +30,32 @@ export default function Home() {
           <h1>News about the <span>React</span> world.</h1>
           <p>
             Get access to all the publications <br />
-            <span>for $11.99 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <Image src={girlCoding} alt="Girl Coding" />
       </main>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1KntziAweduqGJCkprcNeeMw')
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100)
+  }
+
+  return { 
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24, // 24 horas
+  }
 }
